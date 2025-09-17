03ffe082e760960b820ea5a2074b0ad2ed538c8e
today <48009043+uenglish@users.noreply.github.com>
2025-09-17 18:22:13 +0800

    Update Kantian.js

--- Js/Kantian.js
+++ Js/Kantian.js
@@ -2,9 +2,9 @@ var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
 
-obj.msg。isVip = 1;
-obj.msg。isLifeVip = null;
-obj.msg。vipStart = "2025-09-17";
+obj.msg.isVip = 1;
+obj.msg.isLifeVip = null;
+obj.msg.vipStart = "2025-09-17";
 obj.msg.vipEnd = "2099-12-31";
 
 body = JSON.stringify(obj);
